c586639ea126f29ecad7caead4a7bd0483edee2b
Odie <odko_undead@yahoo.com>
2018-01-24 00:20:21 -0600

    While You Wait

---
+++ WhileYouWait/whileyouwait.js
@@ -0,0 +1,25 @@
+var daysUntilMyBirthday = 60;
+for (var i = daysUntilMyBirthday; i >= 0; i--) {
+  if (i >= 30) {
+    console.log("",i, "days until my birthday. Such a long time...:(");
+  }
+    else if (i < 30 && i >=5) {
+      console.log("",i, "days until my birthday. Almost there :)");
+    }
+    else if (i < 5 && i >2) {
+      console.log("",i, "days until my birthday. SCREAM :)");
+    }
+
+    else if (i > 0) {
+      console.log("",i, "DAYS UNTIL MY BIRTHDAY!!!");
+    }
+  else {
+    console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*")
+    console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*")
+    console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪")
+    console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪")
+    console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*")
+    console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*")
+  }
+
+}
