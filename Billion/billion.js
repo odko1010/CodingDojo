caf638dbf573091756decccec709f81a504eda09
Odie <odko_undead@yahoo.com>
2018-01-24 22:20:56 -0600

    billion

---
+++ Billion/billion.js
@@ -0,0 +1,8 @@
+var days = 0;
+for (var reward = 0.01; reward <10000000000; reward *= 2)
+{
+  days++;
+  console.log("day",days,"reward", reward);
+}
+console.log("day 21 reward 10485.76");
+console.log("day 38 reward 1374389534.72");
