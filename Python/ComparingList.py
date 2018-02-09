0ad7cfbe5ae0f00e74cede5c0682d8f2e13ea750
Odie <odko_undead@yahoo.com>
2018-02-08 22:24:57 -0600

    comparing list

---
+++ Python/ComparingList.py
@@ -0,0 +1,6 @@
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','cream']
+if cmp(list_one, list_two) == 0:
+    print "The lists are the same"
+else:
+    print "The lists are not the same"
